e391cd5febfe3cffdb41c835fa0eb32731dbc5cc
Veronika Jezikova <veronikajezikova@Veronika-iMac.local>
2023-01-05 18:46:04 +0100

    editing users profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/profile'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  get "/u/:id", to: "users#profile", as: "user"
+
+
   resources :entries
 
   get "about", to: "pages#about"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_05_165833) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_05_172405) do
   create_table "entries", force: :cascade do |t|
     t.string "name"
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_165833) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "views", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
