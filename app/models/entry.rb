9a0bdf359e24b16ce2c8719ee32b41feafb303ff
Veronika Jezikova <veronikajezikova@Veronika-iMac.local>
2023-01-05 16:09:42 +0100

    editing entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+    validates :name, presence: true
+    validates :title, presence: true
+    validates :body, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+5.times do |x|
+
+    Entry.create(name: "Jane Doe", title: "Téma sezení", session: "#{x}", body: "Záznam ze sezení číslo #{x}.")
+end
