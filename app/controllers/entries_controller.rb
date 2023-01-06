a7e44d485ffa7e70473111c1744303ccde4966cc
Veronika Jezikova <veronikajezikova@Veronika-MacBook-Air.local>
2023-01-06 11:49:12 +0100

    en to cz changes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -29,7 +29,7 @@ class EntriesController < ApplicationController
 
     respond_to do |format|
       if @entry.save
-        format.html { redirect_to entry_url(@entry), notice: "Entry was successfully created." }
+        format.html { redirect_to entry_url(@entry), notice: "Zápis byl úspěšně vytvořen." }
         format.json { render :show, status: :created, location: @entry }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class EntriesController < ApplicationController
   def update
     respond_to do |format|
       if @entry.update(entry_params)
-        format.html { redirect_to entry_url(@entry), notice: "Entry was successfully updated." }
+        format.html { redirect_to entry_url(@entry), notice: "Zápis byl úspěšně aktualizován." }
         format.json { render :show, status: :ok, location: @entry }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class EntriesController < ApplicationController
     @entry.destroy
 
     respond_to do |format|
-      format.html { redirect_to entries_url, notice: "Entry was successfully destroyed." }
+      format.html { redirect_to entries_url, notice: "Zápis byl úspěšně smazán." }
       format.json { head :no_content }
     end
   end
