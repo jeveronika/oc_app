f513ebd624eaa9e9388e4dfe8330633393461638
Veronika Jezikova <veronikajezikova@Veronika-iMac.local>
2023-01-21 13:44:21 +0100

    výpočet průměru a délky všech sezení daného uživatele

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,4 +1,16 @@
 class OverviewController < ApplicationController
     def index
+        @entries = Entry.where(user_id: current_user.id)
+        statistics
     end
+
+    def statistics
+        @entries = Entry.where(user_id: current_user.id)
+        @average_length = @entries.average(:length)
+        # @median_length = @entries.median(:length)
+        @longest_entry = @entries.maximum(:length)
+        @shortest_entry = @entries.minimum(:length)
+      end
+      
+    
 end
