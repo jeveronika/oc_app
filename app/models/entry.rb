52d2145e8ce5a5791763151badaecb8497b30956
Veronika Jezikova <veronikajezikova@Veronika-iMac.local>
2023-01-21 22:10:53 +0100

    chybové hlášky

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,13 +1,14 @@
 class Entry < ApplicationRecord
-    # include ActiveRecord::Calculations
 
-    validates :name, presence: true
-    validates :title, presence: true
-    validates :body, presence: true
+    validates :name, presence: { message: ": jméno nesmí být prázdné" }
+
+    validates :title, presence: { message: ": téma nesmí být prázdné" }
+
+    validates :body, presence: { message: ": zápis sezení nesmí být prázdný" }
     
-    validates :length, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 15, less_than_or_equal_to: 120 }
+    validates :length, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 15, less_than_or_equal_to: 120, message: ": je třeba vyplnit délku sezení" }
 
-    validates :session, numericality: { greater_than_or_equal_to: 0, only_integer: true}
+    validates :session, numericality: { greater_than_or_equal_to: 0, only_integer: true, message: ": je třeba vyplnit číslo sezení"}
 
 
 
